11bb54dc867a152c124512ddc322fad7ab1ce08b
Utkin Denis <stolen.youth@yandex.ru>
2022-06-24 21:00:18 +0700

    feat: some frontend logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Container from "@mui/material/Container";
-
+import { Routes, Route } from 'react-router-dom';
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 
@@ -8,11 +8,13 @@ function App() {
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/posts/:id' element={<FullPost />} />
+              <Route path='/add-post' element={<AddPost />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Registration />} />
+          </Routes>
       </Container>
     </>
   );
